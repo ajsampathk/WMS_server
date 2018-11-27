f69ced72a378d83538e7fb2e3f838c354bf8b684
Aditya V Patil <aditya.vpatil98@gmail.com>
2018-11-27 15:14:08 +0530

    index bug

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express= require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-var levels = require('./Levels.js')
-var ths = require('./Threshold.js')
+// var levels = require('./Levels.js')
+// var ths = require('./Threshold.js')
 app.get('/',(req,res) => {
   res.send('Online')
 })
@@ -12,5 +12,5 @@ app.use(bodyParser.urlencoded({ extended: false}))
 
 app.listen(8000,()=>{console.log("Server started")})
 
-app.use('/level',levels)
-app.use('/ths',ths)
+// app.use('/level',levels)
+// app.use('/ths',ths)
