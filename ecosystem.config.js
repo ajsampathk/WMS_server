4277b536c62c2adbb3ad30a5160b54d48d71489a
Aditya V Patil <aditya.vpatil98@gmail.com>
2018-12-05 18:23:48 +0530

    system change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-216-97-95.us-east-2.compute.amazonaws.com',
+      host: 'ec2-3-16-147-113.us-east-2.compute.amazonaws.com',
       key: '~/Downloads/watermanage.pem',
       ref: 'origin/master',
       repo: 'git@github.com:ajsampathk/WMS_server.git',
